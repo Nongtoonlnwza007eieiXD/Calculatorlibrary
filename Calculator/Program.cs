ff8f598bf3ee0fdeefe581c42d84fef30d559373
Salinatooninter <66030284@kmitl.ac.th>
2024-10-18 11:09:20 +0700

    initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator;
+using Calculatorlibrary;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 2;
+        int b = 3;
+        int c = Calculatorlibrary.Add(a,b);
+        Console.WriteLine($"Result from Add({a},{b})is {c}");
+    }
+}
---
+++ Calculatorlibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculatorlibrary;
+
+public static class Calculatorlibrary
+{
+    public static int Add(int a, int b)
+    {
+        return a+b;
+    }
+}
